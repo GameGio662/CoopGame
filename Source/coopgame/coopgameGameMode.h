36e8736cca795d71d1b0333026e8548e318b611d
GameGio <iiritanogiovanni@gmail.com>
2025-06-10 11:48:49 +0200

    first commit

---
+++ Source/coopgame/coopgameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "coopgameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AcoopgameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AcoopgameGameMode();
+};
+
+
+
